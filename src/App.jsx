967ec8d938015f8f67d6cb7604869bdbef0a49f5
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2023-11-13 16:20:33 -0500

    Contexto de errores de los usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { AuthProvider } from "./context/AuthContext"
+
 import LoginPage from "./pages/LoginPage"
 import RegisterPage from "./pages/RegisterPage"
 
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<h1>Page</h1>} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/posts' element={<h1>Page</h1>} />
-        <Route path='/add-post' element={<h1>Page</h1>} />
-        <Route path='/posts/:id' element={<h1>Page</h1>} />
-        <Route path='/profile' element={<h1>Page</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<h1>Page</h1>} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/posts' element={<h1>Page</h1>} />
+          <Route path='/add-post' element={<h1>Page</h1>} />
+          <Route path='/posts/:id' element={<h1>Page</h1>} />
+          <Route path='/profile' element={<h1>Page</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,18 +1,31 @@
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
-import { registerRequest } from "../api/auth";
+import { useAuth } from "../context/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function RegisterPage() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState:{errors} } = useForm();
+  const { signUp, isAuthenticated, errors: registerErrors } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() =>{
+    if (isAuthenticated) navigate('/posts')
+  },[isAuthenticated] )
 
   const onSubmit = handleSubmit(async (values) => {
-    const res = await registerRequest(values);
-    console.log(res);
+    signUp(values)
   });
 
   return (
     <div className="flex justify-center items-center h-[calc(100vh-100px)]">
       <div className="bg-slate-100 max-w-md w-full p-10 rounded-md">
+        {
+          registerErrors.map((error,i) => (
+            <div className="bg-red-500 p-2 text-white" key ={i}>
+              {error}
+            </div>
+          ))
+        }
         <form onSubmit={onSubmit}>
           <input
             className="w-full bg-slate-200 rounded-md p-2 m-2"
@@ -20,37 +33,60 @@ function RegisterPage() {
             {...register("username", { required: true })}
             placeholder="Username"
           />
+          {
+            errors.username && (
+              <p className="text-red-500">Username is required</p>
+          )}
           <input
             className="w-full bg-slate-200 rounded-md p-2 m-2"
             type="email"
             {...register("email", { required: true })}
             placeholder="Email"
           />
+          {
+            errors.email && (
+              <p className="text-red-500">Email is required</p>
+          )}
           <input
             className="w-full bg-slate-200 rounded-md p-2 m-2"
             type="password"
             {...register("password", { required: true })}
             placeholder="Password"
           />
+          {
+            errors.password && (
+              <p className="text-red-500">Password is required</p>
+          )}
           <input
             className="w-full bg-slate-200 rounded-md p-2 m-2"
             type="password"
             {...register("confirm-password", { required: true })}
             placeholder="Confirm password"
           />
+          {
+            errors.password && (
+              <p className="text-red-500"> Confirm password is required</p>
+          )}
           <input
             className="w-full bg-slate-200 rounded-md p-2 m-2"
             type="text"
             {...register("phone", { required: true })}
             placeholder="Phone"
           />
+          {
+            errors.phone && (
+              <p className="text-red-500">Phone is required</p>
+          )}
           <input
             className="w-full bg-slate-200 rounded-md p-2 m-2"
             type="text"
             {...register("address", { required: true })}
             placeholder="Address"
           />
-
+          {
+            errors.address && (
+              <p className="text-red-500">Address is required</p>
+          )}
           <button
             className="bg-green-700 text-white rounded-md p-2 m-4 mx-2 font-bold hover:bg-green-600"
             type="submit"
