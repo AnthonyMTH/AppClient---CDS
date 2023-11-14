3af562ad3efc28bd69a4f3e37868b817a54f22ab
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2023-11-14 18:26:13 -0500

    Protegiendo las rutas con un validator Token

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,28 @@ import { AuthProvider } from "./context/AuthContext"
 
 import LoginPage from "./pages/LoginPage"
 import RegisterPage from "./pages/RegisterPage"
+import HomePage from "./pages/HomePage"
+import PostsPage from "./pages/PostsPage"
+import PostFormPage from "./pages/PostForm"
+import ProfilePage from "./pages/ProfilePage"
+import ProtectedRoute from "./ProtectedRoute"
+
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<h1>Page</h1>} />
+          <Route path='/' element={<HomePage/>} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />} />
-          <Route path='/posts' element={<h1>Page</h1>} />
-          <Route path='/add-post' element={<h1>Page</h1>} />
-          <Route path='/posts/:id' element={<h1>Page</h1>} />
-          <Route path='/profile' element={<h1>Page</h1>} />
+
+          <Route element={<ProtectedRoute/>}>
+            <Route path='/posts' element={<PostsPage/>} />
+            <Route path='/add-post' element={<PostFormPage/>} />
+            <Route path='/posts/:id' element={<PostFormPage/>} />
+            <Route path='/profile' element={<ProfilePage/>} />
+          </Route>  
         </Routes>
       </BrowserRouter>
     </AuthProvider>
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react"
-import { registerRequest, loginRequest } from "../api/auth";
+import { registerRequest, loginRequest, verifyTokenRequest } from "../api/auth";
+import Cookies from "js-cookie";
 
 export const AuthContext = createContext()
 
@@ -15,7 +16,7 @@ export const AuthProvider =({children}) => {
 	const [user, setUser] = useState(null)
 	const [isAuthenticated, setIsAuthenticated] = useState(false)
 	const [errors, setErrors] = useState([]);
-
+	const [loading, setLoading] = useState(true);
 	const signUp = async (user) =>{	
 		try{
 			const res = await registerRequest(user)
@@ -30,7 +31,9 @@ export const AuthProvider =({children}) => {
 	const signIn = async (user) => {
 		try {
 			const res = await loginRequest(user)	
-			console.log(res)		
+			console.log(res)
+			setUser(res.data)	
+			setIsAuthenticated(true)
 		} catch (error) {
 			if (Array.isArray(error.response.data)) {
 				return setErrors(error.response.data)
@@ -48,12 +51,43 @@ export const AuthProvider =({children}) => {
 		}
 	}, [errors])
 
+	useEffect(() => {
+		async function checkLogin() {
+			const cookies = Cookies.get()
+			if (!cookies.token) {
+				setIsAuthenticated(false);
+				setLoading(false)
+				return setUser(null)
+			}
+
+			try {
+				const res = await verifyTokenRequest(cookies.token)
+				if (!res.data) {
+					setIsAuthenticated(false)
+					setLoading(false)
+					return
+				}
+
+				setIsAuthenticated(true)
+				setUser(res.data)
+				setLoading(false)
+			} catch {
+				console.log(error)
+				setIsAuthenticated(false)
+				setUser(null)
+				setLoading(false)
+			}
+		}
+		checkLogin();
+	}, [])
+
 	return (
 		<AuthContext.Provider 
 		value={{
 			signUp,
 			signIn,
 			user,
+			loading,
 			isAuthenticated,
 			errors
 		}}>
