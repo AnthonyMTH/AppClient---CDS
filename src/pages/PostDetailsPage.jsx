020dd5df6df96dc24f8f53878225e42a51a9582a
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2023-11-18 15:59:00 -0500

    Pagina del Perfil

---
+++ src/pages/PostDetailsPage.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { usePosts } from "../context/PostContext";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useAuth } from "../context/AuthContext";
+
+ function PostDetailsPage(){
+	const { user, isAuthenticated } = useAuth()
+	const { register, setValue } = useForm()
+  	const { getPost, posts } = usePosts()
+	const [ post, setpost ] = useState([])
+	const [ verificator, setverificator] = useState([])
+	const params = useParams()
+
+	useEffect( ()  => {
+		async function loadPost(){
+			const post_ = await getPost(params.id)
+			setpost(post_)
+		}
+		async function verificatorPost(){
+			setverificator(false)
+			const post_ = await getPost(params.id)
+			if(post_.user == user.id){
+				setverificator(true)
+			}
+		}
+		loadPost()
+		verificatorPost()
+	}, []);
+
+  return (
+		<div className="m-5">
+			<div className="border p-5">
+				<div className="flex justify-between">
+					<h1 className="font-bold text-3xl">Descripción</h1>
+					{verificator ? (
+						<>
+						<div>
+							<Link to={`/posts/`} className="rounded-lg border border-blue-300 text-blue-300 p-2 hover:bg-blue-300 hover:text-white">
+							Editar
+							</Link>
+							<Link to={`/posts/`} className="rounded-lg border border-blue-300 text-blue-300 p-2 ml-2 hover:bg-blue-300 hover:text-white">
+							Eliminar
+							</Link>
+						</div>
+						</>
+					) : (<></>)}
+      			</div>
+				<div className="flex justify-between">
+					<p className="m-5">{post.description}</p>
+				</div>
+			</div>
+			<div className="flex justify-center mt-10 ">
+				{verificator ? (<></>
+				) : (<>
+				<Link to={`/posts/`} className="rounded-lg border border-cyan-600 text-cyan-600 p-2 hover:bg-cyan-600 hover:text-white">
+           			Contactar
+          		</Link>
+				</>)}
+			</div>
+		</div>
+	)
+}
+
+export default PostDetailsPage
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,6 +1,75 @@
+import { useAuth } from "../context/AuthContext"
+
 function ProfilePage(){
+    const { user, isAuthenticated } = useAuth()
+    console.log(user)
+
     return (
-        <div>ProfilePage</div>
+        <div className="flex flex-col">
+            <div className="bg-blue-300 flex max-h-full items-end">
+                <div className="bg-green-200 flex h-72 w-72 flex-col justify-baseline ">
+                    <div className="bg-red-200 m-2 flex flex-col justify-between">
+                        <h1 className="flex justify-center">{user.username}</h1>
+                    </div>
+                    <div className="bg-red-200 mr-2 ml-2 h-52 flex flex-col justify-between">
+                        <h1 className="flex justify-center">Foto Personal</h1>
+                    </div>
+                    <div className="bg-red-200 m-2 flex flex-col justify-between">
+                        <h1 className="flex justify-center">Editar Esto sera boton</h1>
+                    </div>
+                </div>
+                <div className="bg-green-900 flex flex-col h-72 w-full">
+                    <div className="bg-red-900 m-2 flex justify-between ">
+                        <h1>Datos Personales:</h1>
+                    </div>
+                    <div className="flex flex-col justify-between m-2">
+                        <div className="flex justify-between">
+                            <div className="bg-red-500 m-2 flex items-start">
+                                <h1>Email: {user.email}</h1>
+                            </div>
+                        </div>
+                        <div className="flex justify-between">
+                            <div className="bg-red-500 m-2 flex items-start">
+                                <h1>Dirección: {user.address}</h1>
+                            </div>
+                        </div>
+                        <div className="flex justify-between">
+                            <div className="bg-red-500 m-2 flex items-start">
+                                <h1>Telefono: {user.phone}</h1>
+                            </div>
+                        </div>
+                        <div className="flex justify-between">
+                            <div className="m-2 flex items-start">
+                                <h1>Tico no tengo creatividad GG :'v</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-red-300 flex-1 justify-center">
+                <div className="m-2 flex flex-col justify-between">
+                    <div className="flex justify-center">
+                        <div className="bg-blue-200 px-2 flex justify-center">
+                            <h1 className="flex justify-center">Fotos de mis mascotas</h1>
+                        </div>
+                    </div>
+                    <div className="flex flex-row justify-between">
+                        <div className="bg-blue-200 m-2 h-48 w-64 flex-1 flex justify-center">
+                            <h1>Foto 1</h1>
+                        </div>
+                        <div className="bg-blue-200 m-2 h-48 w-64 flex-1 flex justify-center">
+                            <h1>Foto 2</h1>
+                        </div>
+                        <div className="bg-blue-200 m-2 h-48 w-64 flex-1 flex justify-center">
+                            <h1>Foto 3</h1>
+                        </div>
+                        <div className="bg-blue-200 m-2 h-48 w-64 flex-1 flex justify-center">
+                            <h1>Foto 4</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>          
+        </div>
     )
 }
 
